441f2bd990b7385cff449a346ee625fd69c7f48c
sinbeom <kanian16@naver.com>
2025-01-30 21:32:42 +0900

    page ui

--- src/app/(user)/signup/info/page.tsx
+++ src/app/(user)/signup/info/page.tsx
@@ -1,15 +1,10 @@
-import InputField from "@/ui/common/input/inputField";
 import Line from "@/ui/common/textNLine/line";
-import React from "react";
-import { useFormContext } from "react-hook-form";
 
 const SignupInfoPage = () => {
-  const {} = useFormContext();
   return (
     <div className="w-full">
       <p className="text-4xl">필수</p>
       <Line />
-      <InputField />
     </div>
   );
 };
